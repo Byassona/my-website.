5bb771452fd5f9d10758119369850ec8742dc49f
Byassona <ustarhanovbilal@gmail.com>
2024-12-18 16:32:47 +0300

    Update register.js

--- register.js
+++ register.js
@@ -3,21 +3,20 @@ document.getElementById("registerForm").addEventListener("submit", (event) => {
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    const email = document.getElementById("email").value;
 
-    // Проверяем, существует ли уже пользователь с таким именем
-    const existingUser = JSON.parse(localStorage.getItem("users")) || [];
-    const userExists = existingUser.some(user => user.username === username);
+    // Получаем список зарегистрированных пользователей из localStorage
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
-    if (userExists) {
-        alert("Пользователь с таким именем уже существует.");
+    // Проверяем, есть ли уже такой пользователь
+    const existingUser = users.find(user => user.username === username);
+
+    if (existingUser) {
+        alert("Этот пользователь уже существует!");
     } else {
         // Добавляем нового пользователя
-        const newUser = { username, password, email };
-        existingUser.push(newUser);
-        localStorage.setItem("users", JSON.stringify(existingUser));
-
-        alert("Вы успешно зарегистрированы!");
+        users.push({ username, password });
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("Регистрация успешна! Теперь вы можете авторизоваться.");
         window.location.href = "login.html"; // Перенаправляем на страницу входа
     }
 });
