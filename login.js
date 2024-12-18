919f7da5b0756c55909b34c626957d18605e0e9a
Byassona <ustarhanovbilal@gmail.com>
2024-12-18 23:17:37 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loggedInUser = localStorage.getItem("loggedInUser");
-    
-    if (loggedInUser) {
-        // Если пользователь уже авторизован, перенаправляем его на главную страницу
-        window.location.href = "index.html";
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Останавливаем стандартную отправку формы
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    // Находим пользователя по имени и паролю
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        // Если пользователь найден, сохраняем информацию о том, что пользователь авторизован
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "index.html"; // Перенаправляем на главную страницу
+    } else {
+        alert("Неверное имя пользователя или пароль.");
     }
 });
