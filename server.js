48a206ef46771de549d73fa8538703bd835b1768
Byassona <ustarhanovbilal@gmail.com>
2024-12-19 14:27:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const stripe = require('stripe')('YOUR_SECRET_KEY'); // Секретный ключ Stripe
+const stripe = require('stripe')('YOUR_SECRET_KEY'); // Ваш секретный ключ Stripe
 const app = express();
 
 app.use(express.static('public'));
