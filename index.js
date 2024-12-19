2e78b3cd73b2ac701d3363572eddc1296122172d
Byassona <ustarhanovbilal@gmail.com>
2024-12-19 13:48:10 +0300

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("logoutButton").addEventListener("click", () => {
-    localStorage.removeItem("loggedInUser"); // Удаляем данные о пользователе
-    window.location.href = "login.html"; // Перенаправляем на страницу авторизации
-});
