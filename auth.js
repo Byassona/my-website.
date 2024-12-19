57842e36c27034a539352aa442ec491bdae11ab6
Byassona <ustarhanovbilal@gmail.com>
2024-12-19 14:48:11 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -9,5 +9,18 @@ document.getElementById('logout')?.addEventListener('click', () => {
   localStorage.removeItem('isLoggedIn');
   localStorage.removeItem('username');
   alert('Вы вышли из системы!');
+  window.location.href = 'login.html'
+
+  // Проверяем статус пользователя
+const username = localStorage.getItem('username');
+const storedUser = JSON.parse(localStorage.getItem(username));
+
+// Если пользователь админ, он получает полный доступ
+const isAdmin = storedUser?.isAdmin || false;
+localStorage.setItem('isAdmin', isAdmin);
+
+if (!localStorage.getItem('isLoggedIn')) {
+  alert('Сначала войдите или зарегистрируйтесь!');
   window.location.href = 'login.html';
+}
 });
